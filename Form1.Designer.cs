ad6d567beccf12de3a5a0aa12dfe15b10ed50918
Clever Coder <clevercoderr@gmail.com>
2022-01-06 02:47:10 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Google_Translate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.Font = new System.Drawing.Font("Engravers MT", 8.920354F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(701, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(570, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(477, 432);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 36);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(482, 432);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "af   ",
+            "sq   ",
+            "am   ",
+            "ar   ",
+            "hy   ",
+            "az   ",
+            "eu   ",
+            "be   ",
+            "bn   ",
+            "bs   ",
+            "bg   ",
+            "ca   ",
+            "ceb  ",
+            "ny   ",
+            "zh-CN",
+            "co   ",
+            "hr   ",
+            "cs   ",
+            "da   ",
+            "nl   ",
+            "en   ",
+            "eo   ",
+            "et   ",
+            "tl   ",
+            "fi   ",
+            "fr   ",
+            "fy   ",
+            "gl   ",
+            "ka   ",
+            "de   ",
+            "el   ",
+            "gu   ",
+            "ht   ",
+            "ha   ",
+            "haw  ",
+            "iw   ",
+            "hi   ",
+            "hmn  ",
+            "hu   ",
+            "is   ",
+            "ig   ",
+            "id   ",
+            "ga   ",
+            "it   ",
+            "ja   ",
+            "jw   ",
+            "kn   ",
+            "kk   ",
+            "km   ",
+            "ko   ",
+            "ku   ",
+            "ky   ",
+            "lo   ",
+            "la   ",
+            "lv   ",
+            "lt   ",
+            "lb   ",
+            "mk   ",
+            "mg   ",
+            "ms   ",
+            "ml   ",
+            "mt   ",
+            "mi   ",
+            "mr   ",
+            "mn   ",
+            "my   ",
+            "ne   ",
+            "no   ",
+            "ps   ",
+            "fa   ",
+            "pl   ",
+            "pt   ",
+            "pa   ",
+            "ro   ",
+            "ru   ",
+            "sm   ",
+            "gd   ",
+            "sr   ",
+            "st   ",
+            "sn   ",
+            "sd   ",
+            "si   ",
+            "sk   ",
+            "sl   ",
+            "so   ",
+            "es   ",
+            "su   ",
+            "sw   ",
+            "sv   ",
+            "tg   ",
+            "ta   ",
+            "te   ",
+            "th   ",
+            "tr   ",
+            "uk   ",
+            "ur   ",
+            "uz   ",
+            "vi   ",
+            "cy   ",
+            "xh   ",
+            "yi   ",
+            "yo   ",
+            "zu   "});
+            this.comboBox1.Location = new System.Drawing.Point(328, 548);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Select Language";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "af   ",
+            "sq   ",
+            "am   ",
+            "ar   ",
+            "hy   ",
+            "az   ",
+            "eu   ",
+            "be   ",
+            "bn   ",
+            "bs   ",
+            "bg   ",
+            "ca   ",
+            "ceb  ",
+            "ny   ",
+            "zh-CN",
+            "co   ",
+            "hr   ",
+            "cs   ",
+            "da   ",
+            "nl   ",
+            "en   ",
+            "eo   ",
+            "et   ",
+            "tl   ",
+            "fi   ",
+            "fr   ",
+            "fy   ",
+            "gl   ",
+            "ka   ",
+            "de   ",
+            "el   ",
+            "gu   ",
+            "ht   ",
+            "ha   ",
+            "haw  ",
+            "iw   ",
+            "hi   ",
+            "hmn  ",
+            "hu   ",
+            "is   ",
+            "ig   ",
+            "id   ",
+            "ga   ",
+            "it   ",
+            "ja   ",
+            "jw   ",
+            "kn   ",
+            "kk   ",
+            "km   ",
+            "ko   ",
+            "ku   ",
+            "ky   ",
+            "lo   ",
+            "la   ",
+            "lv   ",
+            "lt   ",
+            "lb   ",
+            "mk   ",
+            "mg   ",
+            "ms   ",
+            "ml   ",
+            "mt   ",
+            "mi   ",
+            "mr   ",
+            "mn   ",
+            "my   ",
+            "ne   ",
+            "no   ",
+            "ps   ",
+            "fa   ",
+            "pl   ",
+            "pt   ",
+            "pa   ",
+            "ro   ",
+            "ru   ",
+            "sm   ",
+            "gd   ",
+            "sr   ",
+            "st   ",
+            "sn   ",
+            "sd   ",
+            "si   ",
+            "sk   ",
+            "sl   ",
+            "so   ",
+            "es   ",
+            "su   ",
+            "sw   ",
+            "sv   ",
+            "tg   ",
+            "ta   ",
+            "te   ",
+            "th   ",
+            "tr   ",
+            "uk   ",
+            "ur   ",
+            "uz   ",
+            "vi   ",
+            "cy   ",
+            "xh   ",
+            "yi   ",
+            "yo   ",
+            "zu   "});
+            this.comboBox2.Location = new System.Drawing.Point(70, 548);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(222, 27);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = "Select Language";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 666);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        //private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Google_Translate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    
+        //    WebClient webClient = new WebClient();
+        //    webClient.Encoding = Encoding.UTF8;
+        //    try
+        //    {
+        //        string url = String.Format("http://www.google.com/translate_t?hl=en&ie=UTF8&text={0}&langpair={1}", textBox1.Text, comboBox1.Text + "|" + comboBox2.Text);
+        //        string result = webClient.DownloadString(url);
+        //        result = result.Substring(result.IndexOf("<span title=\">") + "<span title=\">".Length);
+        //        result = result.Substring(result.IndexOf(">") + 1);
+        //        result = result.Substring(0, result.IndexOf("</span>"));
+        //        textBox2.Text = result.Trim();
+
+        //        //int bas = result.IndexOf("TRANSLATED_TEXT='") + "TRANSLATED_TEXT='".Length;
+        //        //int bit = result.Substring(bas).IndexOf("';var");
+        //        //result = result.Substring(bas, bit);
+        //        //textBox2.Text = result.Trim();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //    
+        //}
+        */
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+
+            
+            
+            try
+            {
+                string url = string.Format("https://mytransapi.pythonanywhere.com/api/v1/get-only-text/{0}/{1}/?text={2}",
+                    comboBox1.Text.Trim(), comboBox2.Text.Trim(), textBox1.Text.Trim());
+                //string result = webClient.DownloadString(url);
+
+                HttpClient client = new HttpClient();
+                string res = client.GetStringAsync(url).Result;
+                
+                textBox2.Text = res.Trim();
+
+                MessageBox.Show(textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("sda");
+            }
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//result = result.Substring(result.IndexOf("<span title=\"") + "<span title=\"".Length);
+                //result = result.Substring(result.IndexOf(">") + 1);
+                //result = result.Substring(0, result.IndexOf("</span>"));
+                //textBox2.Text = result.Trim();
+
+                //res = res.Substring(res.IndexOf("<span title=\"") + "<span title=\"".Length);
+                //res = res.Substring(res.IndexOf(">") + 1);
+                //res = res.Substring(0, res.IndexOf("</span>"));*/
